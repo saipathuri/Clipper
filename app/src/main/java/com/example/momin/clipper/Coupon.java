12053af82bc5c83ef80374d5a10ff0e0f70d285e
snkmht100 <snkmht100@gmail.com>
2016-01-16 11:23:30 -0600

    coupon, store, listing creation

---
+++ app/src/main/java/com/example/momin/clipper/Coupon.java
@@ -0,0 +1,10 @@
+package com.example.momin.clipper;
+
+/**
+ * Created by Owner on 1/16/16.
+ */
+public class Coupon {
+
+
+
+}
---
+++ app/src/main/java/com/example/momin/clipper/Store.java
@@ -0,0 +1,20 @@
+package com.example.momin.clipper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Owner on 1/16/16.
+ */
+public class Store {
+
+    private String name;
+    private String locationID;
+    private ArrayList<Coupon> coupons;
+
+    public Store(String n) {
+
+    }
+
+
+
+}
