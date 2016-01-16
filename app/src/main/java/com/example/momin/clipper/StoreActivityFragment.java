d9a62d21c2d0a27d7dfcfbb7695abd0d3151a744
mominirfan <Momin.Irfan@live.com>
2016-01-16 13:55:19 -0600

    Starts creating a list for store specific coupons

---
+++ app/src/main/java/com/example/momin/clipper/StoreActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.momin.clipper;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class StoreActivityFragment extends Fragment {
+
+    public StoreActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_store, container, false);
+    }
+}
