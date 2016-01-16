5cc43606befd2459290a30e18389a7152c8d8523
snkmht100 <snkmht100@gmail.com>
2016-01-16 14:06:16 -0600

    query created

--- app/src/main/java/com/example/momin/clipper/StoreLister.java
+++ app/src/main/java/com/example/momin/clipper/StoreLister.java
@@ -7,7 +7,11 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -33,10 +37,48 @@ public class StoreLister {
         else {
 
             double lat = l.getLatitude();
-            double lng = l.getLongitude();
+            double lon = l.getLongitude();
+
+            int mileradius = 2;
+            int displimit = 1000;
+            String orderby = "radius";
 
             String query = "http://api.8coupons.com/v1/getdeals?key=7706d583294296431e81abac1b84d57a3ff88f5b710536108616ae1a0fa4b64919e2fc523bd3a1bdbbab66947a0d67a5";
-            &lat=38.8871&lon=-77.0932&mileradius=2&limit=1000&orderby=radius
+            query += "&lat=" + lat;
+            query += "&lon=" + lon;
+            query += "&mileradius=" + mileradius;
+            query += "&limit=" + displimit;
+            query += "&orderby=" + orderby;
+
+//            DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+//            HttpPost httppost = new HttpPost(http://someJSONUrl/jsonWebService);
+//// Depends on your web service
+//            httppost.setHeader("Content-type", "application/json");
+//
+//            InputStream inputStream = null;
+//            String result = null;
+//            try {
+//                HttpResponse response = httpclient.execute(httppost);
+//                HttpEntity entity = response.getEntity();
+//
+//                inputStream = entity.getContent();
+//                // json is UTF-8 by default
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+//                StringBuilder sb = new StringBuilder();
+//
+//                String line = null;
+//                while ((line = reader.readLine()) != null)
+//                {
+//                    sb.append(line + "\n");
+//                }
+//                result = sb.toString();
+//            } catch (Exception e) {
+//                // Oops
+//            }
+//            finally {
+//                try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+//            }
+
 
 //            try {
 //                List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
