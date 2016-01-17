ee22539191cd9b7f70db4061bd3987164e2889cd
Sai Pathuri <saipathuri@gmail.com>
2016-01-17 03:01:48 -0600

    Update URL

--- app/src/main/java/com/example/momin/clipper/CouponInfo.java
+++ app/src/main/java/com/example/momin/clipper/CouponInfo.java
@@ -3,6 +3,7 @@ package com.example.momin.clipper;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.util.Linkify;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.LinearLayout.*;
@@ -26,15 +27,20 @@ public class CouponInfo extends AppCompatActivity {
 
         TextView dealOrigPrice =new TextView(this);
         dealName=(TextView)findViewById(R.id.dealOrigPrice);
-        dealName.setText("Original Price: $" + String.format("%.2f",Double.toString(display.getOriginalPrice())));
+        dealName.setText("Original Price: $" + String.format("%.2f", display.getOriginalPrice()));
 
         TextView dealNewPrice =new TextView(this);
         dealName=(TextView)findViewById(R.id.dealNewPrice);
-        dealName.setText("Deal Price: $" + String.format("%.2f",Double.toString(display.getDealPrice())));
+        dealName.setText("Deal Price: $" + String.format("%.2f",display.getDealPrice()));
 
         TextView dealMoneySaved = new TextView(this);
         dealName=(TextView)findViewById(R.id.dealMoneySaved);
-        dealName.setText("Money saved: $" + String.format("%.2f",Double.toString((display.getDealSavings()))));
+        dealName.setText("Money saved: $" + String.format("%.2f",(display.getDealSavings())));
+
+        TextView dealURL = new TextView(this);
+        dealURL = (TextView) findViewById(R.id.dealURL);
+        dealURL.setText("Get the deal here: " + display.getUrl());
+        Linkify.addLinks(dealURL, Linkify.WEB_URLS);
     }
 
 
--- app/src/main/java/com/example/momin/clipper/dummy/CouponContent2.java
+++ app/src/main/java/com/example/momin/clipper/dummy/CouponContent2.java
@@ -33,7 +33,7 @@ public class CouponContent2 {
                 ArrayList<Coupon> coupons = x.getCoupons();
                  for (int i = 1; i < coupons.size(); i++) {
                      String title = x.getName() + ": " + coupons.get(i - 1).getDealTitle();
-                     CouponItem2 item = new CouponItem2("$" + String.format("%.2f", Double.toString(coupons.get(i-1).getDealSavings())),
+                     CouponItem2 item = new CouponItem2("$" + String.format("%.2f", coupons.get(i-1).getDealSavings()),
                              title, "", coupons.get(i - 1));
                      addItem(item);
                  }
