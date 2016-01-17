adec37cf8f172b3292cfd2ceca0dcd0aa84dbf22
mominirfan <Momin.Irfan@live.com>
2016-01-17 02:38:18 -0600

    updates bground

--- app/src/main/java/com/example/momin/clipper/ItemFragment.java
+++ app/src/main/java/com/example/momin/clipper/ItemFragment.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.momin.clipper.dummy.StoreContent;
 import com.example.momin.clipper.dummy.StoreContent.StoreItem;
@@ -51,6 +53,7 @@ public class ItemFragment extends Fragment {
         if (getArguments() != null) {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
+
     }
 
     @Override
